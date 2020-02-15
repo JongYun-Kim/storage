acd895f8c7b8bb8c89ef9e191c9a2a390c7e15ca
JongYun-Kim <36770713+JongYun-Kim@users.noreply.github.com>
2020-02-15 23:28:09 +0900

    path loss mean function updated
    (1) Bug in the computation of m resolved

--- MATLAB_files/mean/meanPathloss.m
+++ MATLAB_files/mean/meanPathloss.m
@@ -16,16 +16,16 @@ function [m,dm] = meanPathloss(hyp, x)
 % Copyright (c) by Carl Edward Rasmussen and Hannes Nickisch, 2016-04-15.
 %
 % See also MEANFUNCTIONS.M.
-global base_pos
+global base_pos_meanfunc
 
-% % % if nargin<2, m = '2+D'; return; end             % report number of hyperparameters
-if nargin<2, m = '2'; return; end             % report number of hyperparameters
-[n,D] = size(x);
+% % % if nargin<2, m = '2+D'; return; end       % report number of hyperparameters
+if nargin<2, m = '2'; return; end               % report number of hyperparameters
+[n,~] = size(x);
 % % % if any(size(hyp)~=[2+D,1]), error('Exactly 2+D hyperparameters needed.'), end
 if any(size(hyp)~=[2,1]), error('Exactly 2+D hyperparameters needed.'), end
 %m = x*hyp(:);                                                    % evaluate mean
 %dm = @(q) x'*q(:);
 % % % xb = repmat(hyp(3:end)',n,1);  % == n x D matrix
-xb = repmat(base_pos,n,1);  % == n x D matrix
-m = -10*hyp(1)*log10( sum( (x-xb).^2, 2) ) + hyp(2);  % == n x 1 vector
+xb = repmat(base_pos_meanfunc,n,1);  % == n x D matrix
+m = -10*hyp(1)*log10( sqrt(sum( (x-xb).^2, 2)) ) + hyp(2);  % == n x 1 vector
 dm = @(q) [sum(q), -10*hyp(1)*log10( sum( (x-xb).^2, 2) )'*q(:)];  % 2 x 1 vector
