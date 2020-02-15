ae4d3a9fbb02060fb490cc415c9a43018f5ff822
JongYun-Kim <36770713+JongYun-Kim@users.noreply.github.com>
2020-02-16 00:56:10 +0900

    directional derivatives dm updated
    (1) bug fixed on dm
    (2) more readable code

--- MATLAB_files/mean/meanPathloss.m
+++ MATLAB_files/mean/meanPathloss.m
@@ -1,31 +1,37 @@
 function [m,dm] = meanPathloss(hyp, x)
-
+%
 % Linear mean function. The mean function is parameterized as:
-%
-% m(x) = -10*n*log10( (x-xb).^2 ) + C
-%
+% m(x) = -10*n*log10( sqrt((x-xb).^2) ) + C
 %
 % The hyperparameter is:
-%
 % hyp = [ n
 %         C
-%         x_b1
-%         ..
-%         x_bD ]
+%         %x_b1
+%         %..
+%         %x_bD ]   => base position in hyp outdated
 %
-% Copyright (c) by Carl Edward Rasmussen and Hannes Nickisch, 2016-04-15.
-%
+% Copyright (c) by Jongyun Kim, 2020-02-16.
 % See also MEANFUNCTIONS.M.
-global base_pos_meanfunc
+%
 
-% % % if nargin<2, m = '2+D'; return; end       % report number of hyperparameters
+% Size check
+    % % % if nargin<2, m = '2+D'; return; end       % report number of hyperparameters
 if nargin<2, m = '2'; return; end               % report number of hyperparameters
 [n,~] = size(x);
-% % % if any(size(hyp)~=[2+D,1]), error('Exactly 2+D hyperparameters needed.'), end
-if any(size(hyp)~=[2,1]), error('Exactly 2+D hyperparameters needed.'), end
-%m = x*hyp(:);                                                    % evaluate mean
-%dm = @(q) x'*q(:);
-% % % xb = repmat(hyp(3:end)',n,1);  % == n x D matrix
+    % % % if any(size(hyp)~=[2+D,1]), error('Exactly 2+D hyperparameters needed.'), end
+if any(size(hyp)~=[2,1]), error('Exactly (2x1) hyperparameters required.'), end
+
+% Base position
+global base_pos_meanfunc
+    % % % xb = repmat(hyp(3:end)',n,1);  % == n x D matrix
 xb = repmat(base_pos_meanfunc,n,1);  % == n x D matrix
+
+% Mean and directional derivatives wrt each hyp
 m = -10*hyp(1)*log10( sqrt(sum( (x-xb).^2, 2)) ) + hyp(2);  % == n x 1 vector
-dm = @(q) [sum(q), -10*hyp(1)*log10( sum( (x-xb).^2, 2) )'*q(:)];  % 2 x 1 vector
+    %dm = @(q) [sum(q), -10*hyp(1)*log10( sum( (x-xb).^2, 2) )'*q(:)];  % 2 x 1 vector
+dm = @(q) dirder(q,x,xb);  % directional derivative
+
+function dhyp = dirder(q,x,xb)
+    h1 =  ( -10*log10( sqrt(sum( (x-xb).^2, 2)) )  )' * q(:);  % (nx1)'*(nx1) = (1xn)*(nx1) = (1x1)
+    h2 =  sum(q(:));  % ones(1,n) * q(:);
+    dhyp = [h1; h2];                                            % (2x1) vector
